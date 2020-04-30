22fb4c38113952bf51a67cf9cd9f517afaf49515
gosimitz <gosimitz@users.noreply.github.com>
2020-04-29 23:19:06 -0600

    completed module on Map pattern matching with functions

--- lib/pattern_matching/maps.ex
+++ lib/pattern_matching/maps.ex
@@ -11,20 +11,24 @@ defmodule PatternMatching.Maps do
   Fix or complete the code to make the tests pass.
   """
 
-  def return_name(_value) do
+  def return_name(%{:name => name}), do: name
 
-  end
+  def has_sides?(%{:sides => _sides}), do: true
 
-  def has_sides?(_value) do
+  def has_sides?(_value), do: false
 
-  end
 
-  def net_change(_value) do
+  def net_change(%{:ending_balance => end_bal, :initial_balance => init_bal}), do: {:ok, end_bal-init_bal}
+  def net_change(_value), do: {:error, "Missing balance information"}
 
-  end
+  def classify_response(%{"success" => true, "token" => token}), do: {:ok, token}
 
-  def classify_response(_response) do
+  def classify_response(%{"success" => false, "messages" => %{"general" => %{"result_code" => -1}}}), do: {:error, :invalid}
+  def classify_response(%{"success" => false, "messages" => %{"general" => %{"result_code" => 3}}}), do: {:error, :retry}
+  def classify_response(%{"success" => false, "account" => %{"status_code" => "3001"}}), do: {:error, :frozen}
 
-  end
+
+
+
 
 end
